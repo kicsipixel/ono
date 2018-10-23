297817fe03a8373ca397081ee3d60a56751aa3b2
Szabolcs Toth <tsz@purzelbaum.hu>
2018-10-23 20:15:42 +0400

    Initial Commit

---
+++ ono/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ono
+//
+//  Created by Szabolcs Toth on 10/23/18.
+//  Copyright © 2018 purzelbaum.hu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
